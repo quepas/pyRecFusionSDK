65ba12fd569be28dd84435263fd561a381a7a494
quepas <kiepas.patryk@gmail.com>
2025-09-07 01:49:15 +0200

    Add documentation and docstrings for RecFusion Vec3(i) and Mat[3|4] classes

--- src/init_matrix.cpp
+++ src/init_matrix.cpp
@@ -7,12 +7,21 @@ using namespace nb::literals;
 using namespace RecFusion;
 
 void init_matrix(nb::module_ &m) {
-  // Mat3
-  nb::class_<Mat3>(m, "_Mat3")
-      .def("__init__",
-           [](Mat3 *t, const nb::ndarray<double, nb::shape<3, 3>> &array) {
-             new (t) Mat3(array.data());
-           })
+  // NOTE: _Mat3 and _Mat4 are only non-public base implementation of their
+  // respective RecFusion classes. The user-friendly classes, Mat3 and Mat4, are
+  // defined in Python, because it is easier to add some functionality that way.
+  //
+  // NOTE: _Mat3 and _Mat4 implement numpy `data` read-write property for
+  // accessing matrix elements.
+  nb::class_<Mat3>(
+      m, "_Mat3",
+      "Non-public base implementation of the 3x3 matrix. Use Mat3 instead.")
+      .def(
+          "__init__",
+          [](Mat3 *t, const nb::ndarray<double, nb::shape<3, 3>> &array) {
+            new (t) Mat3(array.data());
+          },
+          "Construct matrix from 3x3 Numpy array")
       .def_prop_rw(
           "data",
           [](Mat3 &matrix) {
@@ -25,14 +34,21 @@ void init_matrix(nb::module_ &m) {
                 matrix(r, c) = array(r, c);
               }
             }
-          })
-      .def("__matmul__", [](Mat3 &self, Mat3 &other) { return self * other; });
-  // Mat4
-  nb::class_<Mat4>(m, "_Mat4")
-      .def("__init__",
-           [](Mat4 *t, const nb::ndarray<double, nb::shape<4, 4>> &array) {
-             new (t) Mat4(array.data());
-           })
+          },
+          "Access matrix values as Numpy array")
+      .def(
+          "__matmul__", [](Mat3 &self, Mat3 &other) { return self * other; },
+          "Multiply 3x3 matrices");
+
+  nb::class_<Mat4>(
+      m, "_Mat4",
+      "Non-public base implementation of the 4x4 matrix. Use Mat4 instead.")
+      .def(
+          "__init__",
+          [](Mat4 *t, const nb::ndarray<double, nb::shape<4, 4>> &array) {
+            new (t) Mat4(array.data());
+          },
+          "Construct matrix from 4x4 Numpy array")
       .def_prop_rw(
           "data",
           [](Mat4 &matrix) {
@@ -45,8 +61,13 @@ void init_matrix(nb::module_ &m) {
                 matrix(r, c) = array(r, c);
               }
             }
-          })
-      .def("__matmul__", [](Mat4 &self, Mat4 &other) { return self * other; })
-      .def("inverse", &Mat4::inverse)
-      .def_static("from_euler", &Mat4::fromEuler, "rx"_a, "ry"_a, "rz"_a);
+          },
+          "Access matrix values as Numpy array")
+      .def(
+          "__matmul__", [](Mat4 &self, Mat4 &other) { return self * other; },
+          "Multiply two 4x4 matrices")
+      .def("inverse", &Mat4::inverse, "Compute inverse matrix")
+      .def_static("from_euler", &Mat4::fromEuler, "rx"_a, "ry"_a, "rz"_a,
+                  "Convert Euler angles in degrees to a homogeneous "
+                  "transformation matrix.");
 }
--- src/init_vector.cpp
+++ src/init_vector.cpp
@@ -6,28 +6,46 @@ using namespace nb::literals;
 using namespace RecFusion;
 
 void init_vector(nb::module_ &m) {
-  // Vec3
-  nb::class_<Vec3>(m, "_Vec3")
-      .def(nb::init<double, double, double>(), "x"_a, "y"_a, "z"_a)
+  // NOTE: _Vec3 and _Vec3i are only non-public base implementation of
+  // their respective RecFusion classes. The user-friendly classes, Vec3 and
+  // Vec3i, are defined in Python, because it is easier to add some
+  // functionality that way.
+  //
+  // NOTE: _Vec3 and _Vec3i don't implement: data() and operator[]. Instead,
+  // they provide direct access to components using .x, .y, or .z properties.
+  nb::class_<Vec3>(m, "_Vec3",
+                   "Non-public base implementation of the 3D floating-point "
+                   "vector. Use Vec3 instead.")
+      .def(nb::init<double, double, double>(), "x"_a, "y"_a, "z"_a,
+           "Construct 3D vector of floating-point values")
       .def_prop_rw(
           "x", [](Vec3 &vec) { return vec[0]; },
-          [](Vec3 &vec, double value) { vec[0] = value; })
+          [](Vec3 &vec, double value) { vec[0] = value; },
+          "Access X, the first vector component")
       .def_prop_rw(
           "y", [](Vec3 &vec) { return vec[1]; },
-          [](Vec3 &vec, double value) { vec[1] = value; })
+          [](Vec3 &vec, double value) { vec[1] = value; },
+          "Access Y, the second vector component")
       .def_prop_rw(
           "z", [](Vec3 &vec) { return vec[2]; },
-          [](Vec3 &vec, double value) { vec[2] = value; });
-  // Vec3i
-  nb::class_<Vec3i>(m, "_Vec3i")
-      .def(nb::init<int, int, int>(), "x"_a, "y"_a, "z"_a)
+          [](Vec3 &vec, double value) { vec[2] = value; },
+          "Access Z, the third vector component");
+
+  nb::class_<Vec3i>(m, "_Vec3i",
+                    "Non-public base implementation of the 3D integer vector. "
+                    "Use Vec3i instead.")
+      .def(nb::init<int, int, int>(), "x"_a, "y"_a, "z"_a,
+           "Construct 3D vector of integer values")
       .def_prop_rw(
           "x", [](Vec3i &vec) { return vec[0]; },
-          [](Vec3i &vec, int value) { vec[0] = value; })
+          [](Vec3i &vec, int value) { vec[0] = value; },
+          "Access X, the first vector component")
       .def_prop_rw(
           "y", [](Vec3i &vec) { return vec[1]; },
-          [](Vec3i &vec, int value) { vec[1] = value; })
+          [](Vec3i &vec, int value) { vec[1] = value; },
+          "Access Y, the second vector component")
       .def_prop_rw(
           "z", [](Vec3i &vec) { return vec[2]; },
-          [](Vec3i &vec, int value) { vec[2] = value; });
+          [](Vec3i &vec, int value) { vec[2] = value; },
+          "Access Z, the third vector component");
 }
