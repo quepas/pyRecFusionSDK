645fcfda91f5d28b71ca8d873aa37cdc582d0e22
quepas <kiepas.patryk@gmail.com>
2025-09-08 18:34:02 +0200

    Put SDK related functions into the sdk submodule

--- src/init_recfusionsdk.cpp
+++ src/init_recfusionsdk.cpp
@@ -9,19 +9,31 @@ using namespace RecFusion;
 using std::to_string;
 
 void init_recfusionsdk(nb::module_ &m) {
-  m.def("init", &RecFusionSDK::init, "log_filename"_a = "",
-        "Initialize the SDK");
-  m.def("deinit", &RecFusionSDK::deinit, "Deinitialize the SDK");
-  m.def("activate", &RecFusionSDK::activate, "key"_a, "code"_a = "");
-  m.def("valid_license", &RecFusionSDK::validLicense);
-  m.def("token", &RecFusionSDK::token);
-  m.def("major_version", &RecFusionSDK::majorVersion);
-  m.def("minor_version", &RecFusionSDK::minorVersion);
-  m.def("build_version", &RecFusionSDK::buildVersion);
+  auto sdk =
+      m.def_submodule("sdk", "Submodule with the SDK related functionality "
+                             "(e.g. version, lincese...).");
+  sdk.def("init", &RecFusionSDK::init, "log_filename"_a = "",
+          "Initialize the SDK.");
+  sdk.def("deinit", &RecFusionSDK::deinit, "Deinitialize the SDK.");
+  sdk.def("activate", &RecFusionSDK::activate, "key"_a, "code"_a = "",
+          "Activate the SDK license.");
+  sdk.def("valid_license", &RecFusionSDK::validLicense,
+          "Check validity of the SDK license.");
+  sdk.def("token", &RecFusionSDK::token,
+          "Get machine token needed for the offline acivation.");
+  sdk.def("major_version", &RecFusionSDK::majorVersion,
+          "Get SDK major version.");
+  sdk.def("minor_version", &RecFusionSDK::minorVersion,
+          "Get SDK minor version.");
+  sdk.def("build_version", &RecFusionSDK::buildVersion,
+          "Get SDK build version.");
   // New API
-  m.def("version", []() {
-    return to_string(RecFusionSDK::majorVersion()) + "." +
-           to_string(RecFusionSDK::minorVersion()) + "." +
-           to_string(RecFusionSDK::buildVersion());
-  });
+  sdk.def(
+      "version",
+      []() {
+        return to_string(RecFusionSDK::majorVersion()) + "." +
+               to_string(RecFusionSDK::minorVersion()) + "." +
+               to_string(RecFusionSDK::buildVersion());
+      },
+      "Get SDK full version string.");
 }
--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -1,17 +1,17 @@
-from pyRecFusionSDK import build_version, major_version, minor_version, token, version
+from pyRecFusionSDK import sdk
 
 
 class TestRecFusionSDK:
     def test_version(self):
         # NOTE: actually, there is no other SDK version that we could use!
         #       This needs to be updated if a new version is released.
-        assert major_version() == 2
-        assert minor_version() == 2
-        assert build_version() == 0
-        assert version() == "2.2.0"
+        assert sdk.major_version() == 2
+        assert sdk.minor_version() == 2
+        assert sdk.build_version() == 0
+        assert sdk.version() == "2.2.0"
 
     def test_machine_token(self):
-        machine_token = token()
+        machine_token = sdk.token()
         assert len(machine_token) == 32
         # TODO: this could be more strict! Especially, isalpha()
         assert all(map(lambda x: x.isdigit() or x.isalpha(), machine_token))
